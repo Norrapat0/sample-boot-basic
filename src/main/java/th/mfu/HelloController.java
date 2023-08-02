e17c7e6700d15f83481322a1dd5e7786edfa68ef
Norrapat0 <119600305+Norrapat0@users.noreply.github.com>
2023-08-02 14:19:39 +0700

    first

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,5 +1,6 @@
 package th.mfu;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+     @GetMapping("/hello")
     String hello() {
         return "Hello World!";
     }
 
+    @GetMapping("/hi/{name}")
+    String hi(@PathVariable String name) {
+        return "Hello" + name;
+    }
+
+    @GetMapping("/sum/{a}/{b}") 
+    int sum(@PathVariable int a, @PathVariable int b) {
+        return a + b;
+    }
    
+
 }
--- src/test/java/th/mfu/HelloControllerTest.java
+++ src/test/java/th/mfu/HelloControllerTest.java
@@ -20,4 +20,23 @@ public class HelloControllerTest {
         // Assert
         assertEquals("Hello World!", response);
     }
+
+    @Test
+    public void testHi() {
+        // Act
+        String response = controller.hi(" CMU");
+        // Assert
+        assertEquals("Hello CMU", response);
+    }
+
+    @Test
+    public void testSum() {
+        int response = controller.sum(5, 6);
+        assertEquals(11, response);
+        int response1 = controller.sum(4, 2);
+        assertEquals(6, response1);
+        int response2 = controller.sum(3, 4);
+        assertEquals(7, response2);
+    }
+
 }
